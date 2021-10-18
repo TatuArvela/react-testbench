cda5b91eeeefdd5b947c54323e2da43a12832b4e
Tatu Arvela <tatu.arvela@nitor.com>
2021-10-18 17:39:05 +0300

    Fix NavigationLink rendering

--- client/src/app/header/NavigationLink.tsx
+++ client/src/app/header/NavigationLink.tsx
@@ -7,7 +7,7 @@ interface Props {
   children: React.ReactNode;
 }
 
-const StyledLink = styled.a`
+const StyledLink = styled(Link)`
   color: white;
   margin: 10px;
   text-decoration: none;
@@ -19,15 +19,8 @@ const StyledLink = styled.a`
   }
 `;
 
-const ConnectedLink = React.forwardRef((props, ref) => (
-  // @ts-ignore
-  <StyledLink ref={ref} {...props}>
-    {props.children}
-  </StyledLink>
-));
-
 const NavigationLink = ({ to, children }: Props) => (
-  <Link component={ConnectedLink} to={to} children={children} />
+  <StyledLink to={to} children={children} />
 );
 
 export default NavigationLink;
